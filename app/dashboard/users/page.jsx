6be68bc4b59fc35eeec36ad85a50b2111c7c2728
Sang <ptsang1093@gmail.com>
2023-11-19 21:30:14 +0700

    add components for users and products pages

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,5 +1,66 @@
+import Search from '@/app/ui/dashboard/search/search';
+import styles from '../../ui/dashboard/users/users.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import { MdDelete } from 'react-icons/md';
+
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.topContainer}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addBtn}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  width={40}
+                  height={40}
+                  className={styles.image}
+                  alt=""
+                />
+                John Han
+              </div>
+            </td>
+            <td>john@gmail.com</td>
+            <td>10.10.23</td>
+            <td>Admin</td>
+            <td>active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.btn} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <Link href="/">
+                  <button className={`${styles.btn} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </Link>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default UsersPage;
