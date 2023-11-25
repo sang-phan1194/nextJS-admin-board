33a338afb4e1f4e6cee692a6ef3d211a4cc49587
Sang <ptsang1093@gmail.com>
2023-11-25 21:39:06 +0700

    add mongoose and schemas for user and product

---
+++ app/lib/models.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+    min: 3,
+    max: 20
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  img: {
+    type: String
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  phone: {
+    type: String
+  },
+  address: {
+    type: String
+  }
+}, { timestamps: true });
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  desc: {
+    type: String,
+    require: true
+  },
+  price: {
+    type: Number,
+    require: true,
+    min: 0
+  },
+  stock: {
+    type: Number,
+    require: true,
+    min: 0
+  },
+  img: {
+    type: String
+  },
+  color: {
+    type: String
+  },
+  size: {
+    type: String
+  }
+}, { timestamps: true });
+
+export const User = mongoose.model.User || mongoose.model("User", userSchema)
+export const Product = mongoose.model.Product || mongoose.model("Product", productSchema)
